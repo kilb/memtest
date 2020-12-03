0a6d4e2165a9e27fd1e6c006579d9e385d063bc1
kilb <git@bll.io>
2020-12-03 20:44:16 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::{ptr, mem};
-use libc::mmap;
+use libc::{mmap, munmap, c_void};
 use libc::{PROT_READ, PROT_WRITE, MAP_PRIVATE, MAP_ANONYMOUS, MAP_HUGETLB};
 unsafe fn mmap_malloc(len: usize) -> Vec<u8>
 {
     let ptr = mmap(ptr::null_mut(), len, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_HUGETLB, -1,0);
+    println!("{:?}", ptr);
     Vec::from_raw_parts(
         ptr as *mut u8,
         len,
@@ -13,7 +14,8 @@ unsafe fn mmap_malloc(len: usize) -> Vec<u8>
 
 fn main() {
     unsafe {
-        let a = mmap_malloc(1024*1024*1024*2);
+        let mut a = mmap_malloc(1024*1024*1024*2);
         println!("{}", mem::align_of_val(&a));
+        munmap(a.as_mut_ptr() as *mut c_void, a.capacity());
     }
 }
